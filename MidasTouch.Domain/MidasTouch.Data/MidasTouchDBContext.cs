8a9d0842305f67aedbfecc9e0ea2a096842c6eff
GGSimmions1992 <gary.simmons@colorado.edu>
2019-01-30 21:24:10 -0500

    Add DbSets, OnConfiguring, and OnModelCreating to DBContext [#163611027]

--- MidasTouch.Domain/MidasTouch.Data/MidasTouchDBContext.cs
+++ MidasTouch.Domain/MidasTouch.Data/MidasTouchDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MidasTouch.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,21 @@ namespace MidasTouch.Data
 {
     class MidasTouchDBContext: DbContext
     {
+        public DbSet<FinProduct> FinProducts { get; set; }
+        public DbSet<History> Histories { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<User> Users { get; set; }
 
-    }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("server=garydotnet2019.database.windows.net;database=MidasTouchDB;user id=sqladmin;password=Florida2019;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<FinProduct>().HasKey(e => e.Id);
+            builder.Entity<History>().HasKey(e => e.Id);
+            builder.Entity<Portfolio>().HasKey(e => e.Id);
+            builder.Entity<User>().HasKey(e => e.Id);
+        }
 }
