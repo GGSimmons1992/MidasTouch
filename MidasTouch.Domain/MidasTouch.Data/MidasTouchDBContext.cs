86f1ca0627f1b415ba35ddc3ac3c776a1fadf1f9
GGSimmions1992 <gary.simmons@colorado.edu>
2019-01-30 21:15:49 -0500

    Create DBContext file [#163611027]

---
+++ MidasTouch.Domain/MidasTouch.Data/MidasTouchDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidasTouch.Data
+{
+    class MidasTouchDBContext: DbContext
+    {
+
+    }
+}
