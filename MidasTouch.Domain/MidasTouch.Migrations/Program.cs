2487a7e5dae06f75440106c6ed076a7e58974d3c
GGSimmions1992 <gary.simmons@colorado.edu>
2019-01-30 21:07:17 -0500

    Create Data, Migrations, and their NuGet dependancies

---
+++ MidasTouch.Domain/MidasTouch.Migrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MidasTouch.Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
