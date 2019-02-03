1fdfcb5242323e99e5e56f3ee8ae55a8589f1037
Timothy Freeman <freemantimothym@gmail.com>
2019-02-03 14:47:54 -0500

    moved flux method into Ticker class and removed data and migrations projects [#163631703]

--- MidasTouch.Domain/MidasTouch.Domain/Models/Share.cs
+++ MidasTouch.Domain/MidasTouch.Domain/Models/Share.cs
@@ -11,15 +11,5 @@ namespace MidasTouch.Domain.Models
     public int NumberOfShares { get; set; }
     public string Symbol { get; set; }
     public double Price { get; set; }
-
-    public void Flux()
-    {
-      var Change = FinProduct.Beta - 1;
-      var Random = new Random();
-
-      var Delta = Random.NextDouble();
-      Price += Price * (2 * Change * Delta - Change);
-      History.HistoricalData.Add(DateTime.Now, Price);
-    }
   }
 }
--- MidasTouch.Domain/MidasTouch.Domain/Models/Ticker.cs
+++ MidasTouch.Domain/MidasTouch.Domain/Models/Ticker.cs
@@ -1,4 +1,5 @@
 ﻿using MidasTouch.Domain.Abstracts;
+using System;
 
 namespace MidasTouch.Domain.Models
 {
@@ -7,5 +8,13 @@ namespace MidasTouch.Domain.Models
     public string Symbol { get; set; }
     public double Beta { get; set; }
     public Stock Stocks { get; set; }
+
+    public void Flux()
+    {
+      var Change = Beta - 1;
+      var Random = new Random();
+
+      var Delta = Random.NextDouble();
+      Stocks.Price += Stocks.Price * (2 * Change * Delta - Change);    }
   }
 }
