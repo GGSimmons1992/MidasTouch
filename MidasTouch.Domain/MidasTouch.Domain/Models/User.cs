7208efe9eada49c19a1a5a3f8bb306d1706163e7
Timothy Freeman <freemantimothym@gmail.com>
2019-02-03 14:05:04 -0500

    make corrections to the buy and sell methods of the user class [#163631703]

--- MidasTouch.Domain/MidasTouch.Domain/Models/User.cs
+++ MidasTouch.Domain/MidasTouch.Domain/Models/User.cs
@@ -38,41 +38,61 @@ namespace MidasTouch.Domain.Models
 
       t.Stocks.NumberOfStocks -= shares;
       var sh = new Share() { NumberOfShares = shares, Symbol = ticker, Price = t.Stocks.Price };
-
-      if (Portfolio.Shares.Contains(sh))
-      {
-          Portfolio.Shares[] += shares;
-      }
-      else
-      {
-          Portfolio.Stocks.Add(ticker.Ticker, shares);
-      }
-
-      Account -= (ticker.PurchasePrice * shares);
+      AccountBalance -= (sh.Price * shares);
+      Portfolio.Shares.Add(sh);
       return true;
     }
 
-    public bool Sell(FinProduct ticker, int shares)
+    public bool Sell(string name, string ticker, int shares)
     {
-      if (!(Portfolio.Stocks.ContainsKey(ticker.Ticker)))
+      var c = Market.Companies.Where(s => s.Name == name).FirstOrDefault();
+      if (c == null)
+      {
+        return false;
+      }
+
+      var t = c.Tickers.Where(s => s.Symbol == ticker).FirstOrDefault();
+      if (t == null)
+      {
+        return false;
+      }
+
+      var st = Portfolio.Shares.Where(s => s.Symbol == ticker).FirstOrDefault();
+      if (st == null)
       {
           return false;
       }
-      if (Portfolio.Stocks[ticker.Ticker] < shares)
+
+      var sh = Portfolio.Shares.Where(s => s.Symbol == ticker).ToList();
+      var am = 0;
+      foreach (var item in sh)
+      {
+        am += item.NumberOfShares;
+      }
+
+      if (am < shares)
       {
           return false;
       }
 
-      Portfolio.Stocks[ticker.Ticker] -= shares;
-
-      if(Portfolio.Stocks[ticker.Ticker] == 0)
+      while (shares > 0)
       {
-          Portfolio.Stocks.Remove(ticker.Ticker);
+        if (shares < sh[0].NumberOfShares)
+        {
+          sh[0].NumberOfShares -= shares;
+          AccountBalance += (sh[0].Price * shares);
+          t.Stocks.NumberOfStocks += shares;
+          shares = 0;
+        }
+        else
+        {
+          shares -= sh[0].NumberOfShares;
+          t.Stocks.NumberOfStocks += sh[0].NumberOfShares;
+          AccountBalance += (sh[0].NumberOfShares * sh[0].Price);
+          sh.RemoveAt(0);
+        }
       }
 
-      ticker.Shares += shares;
-
-      Account += (ticker.Price * shares);
       return true;
     }
 
