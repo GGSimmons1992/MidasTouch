7373f4e4d2835f6475576fce6349f6ea4f8db017
GGSimmions1992 <gary.simmons@colorado.edu>
2019-02-04 21:41:21 -0500

    Test MarketCap Getter [#163632460]

--- MidasTouch.Domain/MidasTouch.Tests/CompanyTests.cs
+++ MidasTouch.Domain/MidasTouch.Tests/CompanyTests.cs
@@ -2,13 +2,44 @@
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using MidasTouch.Domain.Models;
 
 namespace MidasTouch.Tests
 {
     public class CompanyTests
     {
+        public Company Sut {get;set;}
+        
+        public CompanyTests()
+        {
+            var ticker1=new Ticker()
+            {
+                Stocks=new Stock()
+                {
+                    NumberOfStocks=100,
+                    Price=(double)50m
+                }
+            };
+
+            var ticker2=new Ticker()
+            {
+                Stocks=new Stock()
+                {
+                    NumberOfStocks=300,
+                    Price=(double)10m
+                }
+            };
+
+            Sut= new Company()
+            {
+                Tickers=new List<Ticker>(){ticker1,ticker2}
+            };
+        }
+
         [Fact]
         public void Test_GetCap()
-        { }
+        {
+            Assert.True(Sut.MarketCap== (double)(5000m+3000m));
+        }
     }
 }
