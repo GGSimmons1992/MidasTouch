98819b40a3a7c5fccae90cf88306cab151532017
youtsed <YoutseD@hotmail.com>
2019-02-08 14:08:54 -0500

    Stock Charts Controller & View Added [#163767000]

---
+++ MidasTouch.Domain/MidasTouch.Mvc/Models/StockPriceModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MidasTouch.Mvc.Models
+{
+    public class StockPriceModel
+    {
+        public string Ticker { get; set; }
+        public string Date { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public decimal AdjustedClose { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
