398b659d43cdf03df0c6189e66574d1cd9af1f0f
GGSimmions1992 <gary.simmons@colorado.edu>
2019-02-09 14:16:03 -0500

    Create UserHelper [#163613013]

---
+++ MidasTouch.Domain/MidasTouch.Data/Helpers/UserHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MidasTouch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MidasTouch.Data.Helpers
+{
+    public class UserHelper
+    {
+        private MidasTouchDBContext _db { get; set; }
+
+        public UserHelper()
+        {
+            _db = new MidasTouchDBContext();
+        }
+
+        public long SetUser(User domuser)
+        {
+            _db.Users.Add(domuser);
+            return _db.SaveChanges();
+        }
+
+        public List<User> GetUsers()
+        {
+            var userlist = _db.Users.Include(x => x.Portfolio).Include(y => y.Portfolio).ToList();
+            return userlist;
+        }
+
+    }
+}
