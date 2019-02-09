fea5b371561e29d4019d9edf1e76b635cbf4ceda
GGSimmions1992 <gary.simmons@colorado.edu>
2019-02-09 17:22:46 -0500

    Add SetShare and GetShares [#163613013]

--- MidasTouch.Domain/MidasTouch.Data/Helpers/ShareHelper.cs
+++ MidasTouch.Domain/MidasTouch.Data/Helpers/ShareHelper.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MidasTouch.Domain.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MidasTouch.Data.Helpers
 {
-    class ShareHelper
+    public class ShareHelper
     {
+        private MidasTouchDBContext _db { get; set; }
+
+        public ShareHelper()
+        {
+            _db = new MidasTouchDBContext();
+        }
+
+        public long SetShare(Share share)
+        {
+            _db.Shares.Add(share);
+            return _db.SaveChanges();
+        }
+
+        public List<Share> GetShares()
+        {
+            return _db.Shares.ToList();
+        }
     }
 }
