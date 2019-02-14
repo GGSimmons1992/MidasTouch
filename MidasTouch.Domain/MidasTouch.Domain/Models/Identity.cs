2a864b25df56f50cd6c65ad5fc028e0197e84b21
GGSimmions1992 <gary.simmons@colorado.edu>
2019-02-13 20:40:57 -0500

    Add email and password to user.Identity [#163631703]

--- MidasTouch.Domain/MidasTouch.Domain/Models/Identity.cs
+++ MidasTouch.Domain/MidasTouch.Domain/Models/Identity.cs
@@ -5,11 +5,13 @@ namespace MidasTouch.Domain.Models
   public class Identity : AThing
   {
     public Name Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 
     public override bool IsValid()
     {
       return
-        Name.IsValid();
+        (Name.IsValid() && Validator.ValidateString(this));
     }
   }
 }
