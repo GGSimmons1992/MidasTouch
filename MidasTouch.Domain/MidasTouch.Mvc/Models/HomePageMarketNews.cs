b57052f38fb6c545c5d5fc24a9971a85276c7bbc
youtsed <YoutseD@hotmail.com>
2019-02-14 16:14:13 -0500

    navbar update [#163983001]

---
+++ MidasTouch.Domain/MidasTouch.Mvc/Models/HomePageMarketNews.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MidasTouch.Mvc.Models
+{
+    public class HomePageMarketNews
+    {
+        public DateTime Datetime { get; set; }
+        public string Headline { get; set; }
+        public string Source { get; set; }
+        public string Url { get; set; }
+        public string Summary { get; set; }
+        public string Related { get; set; }
+    }
+}
