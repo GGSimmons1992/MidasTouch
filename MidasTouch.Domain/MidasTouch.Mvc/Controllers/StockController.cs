af65a87bce235dde62ce301ed244994773882803
Timothy Freeman <freemantimothym@gmail.com>
2019-02-14 16:09:45 -0500

    remove sell and buy classes, consolodate to a trade class, write validation and trade method [#163683639]

--- MidasTouch.Domain/MidasTouch.Mvc/Controllers/StockController.cs
+++ MidasTouch.Domain/MidasTouch.Mvc/Controllers/StockController.cs
@@ -1,119 +1,115 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MidasTouch.Data.Helpers;
 using MidasTouch.Mvc.Models;
 
 namespace MidasTouch.Mvc.Controllers
 {
-    public class StockController : Controller
+  public class StockController : Controller
+  {
+    public static double StockPrice { get; set; }
+
+    public IActionResult Stock()
     {
-        public static double StockPrice { get; set; }
-
-        public IActionResult Stock()
-        {
-            return View("Stock");
-        }
-
-        [HttpPost]
-        public IActionResult Stock(string symbol)
-        {
-            var IEXTrading_API_PATH = "https://api.iextrading.com/1.0/stock/{0}/quote";
-
-            IEXTrading_API_PATH = string.Format(IEXTrading_API_PATH, symbol);
-
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-                //For IP-API
-                client.BaseAddress = new Uri(IEXTrading_API_PATH);
-                HttpResponseMessage response = client.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
-                if (response.IsSuccessStatusCode)
-                {
-                    var Stock = response.Content.ReadAsAsync<StockQuote>().GetAwaiter().GetResult();
-
-                    string MarketCapFormatted = Stock.MarketCap.ToString("#,##0");
-
-
-                    ViewBag.Symbol = Stock.Symbol;
-                    ViewBag.CompanyName = Stock.CompanyName;
-                    ViewBag.Price = Stock.LatestPrice;
-                    StockPrice = Stock.LatestPrice;
-                    ViewBag.MarketCap = MarketCapFormatted;
-                    ViewBag.PercentChange = Stock.ChangePercent;
-                }
-
-
-            }
-
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult DisplayStock()
-        {
-            return View("Stock");
-        }
-
-
-        [HttpPost]
-        public IActionResult Buy(string symbol, int buysharescount)
-        {
-            //Delete the lines below when we have a working login --V
-            var testuserlist = (new UserHelper()).GetUsers();
-            var mytestuser = testuserlist[0];
-            HttpContext.Session.SetString("First", mytestuser.Identity.Name.First);
-            HttpContext.Session.SetInt32("userid", mytestuser.Id);
-            //Delete the lines above when we have a working login --^
-
-            var BuyStock = new BuyStock();
-
-            var IEXTrading_API_PATH = "https://api.iextrading.com/1.0/stock/{0}/quote";
-
-            IEXTrading_API_PATH = string.Format(IEXTrading_API_PATH, symbol);
-
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-                //For IP-API
-                client.BaseAddress = new Uri(IEXTrading_API_PATH);
-                HttpResponseMessage response = client.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
-                if (response.IsSuccessStatusCode)
-                {
-                    var Stock = response.Content.ReadAsAsync<BuyStock>().GetAwaiter().GetResult();
-
-                    string MarketCapFormatted = Stock.MarketCap.ToString("#,##0");
-                    
-                    
-
-                    if (Stock.LatestVolume > buysharescount)
-                    {
-                        BuyStock.BuySharesCount = buysharescount;
-
-                        BuyStock.LatestPrice = Stock.LatestPrice;
-                        BuyStock.CompanyName = Stock.CompanyName;
-
-                        var uh = new UserHelper();
-                        var userlist = uh.GetUsers();
-                        var userid = HttpContext.Session.GetInt32("userid");
-
-                        var myuser = userlist.FirstOrDefault(u => u.Id == userid);
-
-                        BuyStock.Buy(symbol, buysharescount, myuser);
-                    }
-
-                }
-            }
-
-            return View("Stock");
-        }
-
-
+      var states = new List<SelectListItem>()
+      {
+        new SelectListItem { Value = "Buy", Text = "Buy" },
+        new SelectListItem { Value = "Sell", Text = "Sell" }
+      };
+      ViewBag.state = states;
+      return View("Stock");
     }
+
+    [HttpPost]
+    public IActionResult Stock(string symbol)
+    {
+      var IEXTrading_API_PATH = "https://api.iextrading.com/1.0/stock/{0}/quote";
+
+      IEXTrading_API_PATH = string.Format(IEXTrading_API_PATH, symbol);
+
+      using (HttpClient client = new HttpClient())
+      {
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+        //For IP-API
+        client.BaseAddress = new Uri(IEXTrading_API_PATH);
+        HttpResponseMessage response = client.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
+        if (response.IsSuccessStatusCode)
+        {
+          var Stock = response.Content.ReadAsAsync<StockQuote>().GetAwaiter().GetResult();
+
+          string MarketCapFormatted = Stock.MarketCap.ToString("#,##0");
+
+          ViewBag.Symbol = Stock.Symbol;
+          ViewBag.CompanyName = Stock.CompanyName;
+          ViewBag.Price = Stock.LatestPrice;
+          StockPrice = Stock.LatestPrice;
+          ViewBag.MarketCap = MarketCapFormatted;
+          ViewBag.PercentChange = Stock.ChangePercent;
+          var states = new List<SelectListItem>()
+          {
+            new SelectListItem { Value = "Buy", Text = "Buy" },
+            new SelectListItem { Value = "Sell", Text = "Sell" }
+          };
+          ViewBag.state = states;
+        }
+      }
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Trade(string symbol, int buysharescount, string state)
+    {
+      //Delete the lines below when we have a working login --V
+      var testuserlist = (new UserHelper()).GetUsers();
+      var mytestuser = testuserlist[0];
+      HttpContext.Session.SetString("First", mytestuser.Identity.Name.First);
+      HttpContext.Session.SetInt32("userid", mytestuser.Id);
+      //Delete the lines above when we have a working login --^
+
+      var IEXTrading_API_PATH = "https://api.iextrading.com/1.0/stock/{0}/quote";
+
+      IEXTrading_API_PATH = string.Format(IEXTrading_API_PATH, symbol);
+
+      using (HttpClient client = new HttpClient())
+      {
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+        //For IP-API
+        client.BaseAddress = new Uri(IEXTrading_API_PATH);
+        HttpResponseMessage response = client.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
+        if (response.IsSuccessStatusCode)
+        {
+          var Stock = response.Content.ReadAsAsync<TradeStock>().GetAwaiter().GetResult();
+
+          var uh = new UserHelper();
+          var userlist = uh.GetUsers();
+          var userid = HttpContext.Session.GetInt32("userid");
+          var myuser = userlist.FirstOrDefault(u => u.Id == userid);
+
+          var TradeStock = new TradeStock()
+          {
+            User = myuser,
+            TradeSharesCount = buysharescount,
+            LatestPrice = Stock.LatestPrice,
+            CompanyName = Stock.CompanyName,
+            Symbol = symbol,
+            State = state,
+            LatestVolume = Stock.LatestVolume
+          };
+
+          TradeStock.Trade();
+        }
+      }
+
+      return View("Stock");
+    }
+  }
 }
