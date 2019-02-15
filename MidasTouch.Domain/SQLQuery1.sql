eef9232dba5320bd645215b7208faeab55f64247
GGSimmions1992 <gary.simmons@colorado.edu>
2019-02-15 11:31:38 -0500

    Change MVC start up to include UseDeveloperExceptionPage() [#164011819]

--- MidasTouch.Domain/SQLQuery1.sql
+++ MidasTouch.Domain/SQLQuery1.sql
@@ -5,8 +5,8 @@ delete from Shares;
 delete from Identities;
 delete from Names;
 
-select First,Last,NameId from Names left outer join Identities on Identities.Id=dbo.Names.Id;
-select Users.IdentityId,Users.PortfolioId from Users left outer join Portfolios on Portfolios.Id=Users.PortfolioId;
+select First,Last,NameId,Identities.Email from Names left outer join Identities on Identities.Id=dbo.Names.Id;
+select Users.IdentityId,Users.PortfolioId,Users.AccountBalance from Users left outer join Portfolios on Portfolios.Id=Users.PortfolioId;
 select * from Shares;
 
 
