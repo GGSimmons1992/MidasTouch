8da168e99a8af3381fc86ef107dbdd5ddf7a8a86
youtsed <YoutseD@hotmail.com>
2019-02-16 15:20:11 -0500

    WIP Dispaly Orders in Accounts [#164029628]

---
+++ MidasTouch.Domain/MidasTouch.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MidasTouch.Mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [HttpGet]
+
+        public async Task<IActionResult> OrderHistory()
+        {
+            //return View(await _db.Portfolio.ToListAsync());
+            return View();
+        }
+    }
+}
---
+++ MidasTouch.Domain/MidasTouch.Mvc/ViewModels/PortfolioViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MidasTouch.Domain.Models;
+
+namespace MidasTouch.Mvc.ViewModels
+{
+    public class PortfolioViewModel
+    {
+        public IEnumerable<User> UserPortfolio { get; set; }
+    }
+}
