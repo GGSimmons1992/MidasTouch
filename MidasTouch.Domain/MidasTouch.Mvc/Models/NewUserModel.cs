d77a97e5d138589104a1afe17dae9b7e889e3bc1
GGSimmions1992 <gary.simmons@colorado.edu>
2019-02-17 03:58:15 -0500

    Add NewUserModel.cs [#164020552]

---
+++ MidasTouch.Domain/MidasTouch.Mvc/Models/NewUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MidasTouch.Domain.Models;
+
+namespace MidasTouch.Mvc.Models
+{
+    public class NewUserModel:User
+    {
+        public string Secondary { get; set; }
+    }
+}
