2403daa4a36fc3856f79c85dd8f443cc3543ad94
GGSimmions1992 <gary.simmons@colorado.edu>
2019-02-18 13:08:26 -0500

    Remove test user from Migrations Program [#164011819]

--- MidasTouch.Domain/MidasTouch.Migration/Program.cs
+++ MidasTouch.Domain/MidasTouch.Migration/Program.cs
@@ -16,46 +16,5 @@ namespace MidasTouch.Migration
             Console.WriteLine("Why are you here?");
         }
 
-        static void CreateTestUser()
-        {
-            var uh = new UserHelper();
-            var Share1 = new Share()
-            {
-                NumberOfShares = 100,
-                Price = 50,
-                Symbol = "GOOG",
-                State = "Buy"
-            };
-
-            var Share2 = new Share()
-            {
-                NumberOfShares = 100,
-                Price = 100,
-                Symbol = "GOOG",
-                State = "Buy"
-            };
-
-            var Sut = new User()
-            {
-                AccountBalance = (double)5000000m,
-                Portfolio = new Portfolio()
-                {
-                    Shares = new List<Share>() { Share1, Share2 }
-                },
-                Identity = new Identity()
-                {
-                    Name = new Name()
-                    {
-                        First = "John",
-                        Last = "Schmidt"
-                    },
-                    Email="John.Schmidt@MidasTouch.com",
-                    Password="607d!"
-                }
-            };
-            uh.SetUser(Sut);
-            Console.WriteLine("Finish Set up");
-        }
-
     }
 }
